e65ad30eb2fd18de5aed6672f995e2dcdb69dc87
yangf96 <fyang96c@gmail.com>
2016-02-01 23:40:33 -0800

    Added Skill, Status

--- projects/vs2013proj/Character.h
+++ projects/vs2013proj/Character.h
@@ -20,14 +20,22 @@ class Hero : public Character
 {
 private:
 	double money;
+	int level;
 	Item* items;
+public:
+	Hero(std::string name, int hp, int att, int def, int spd, double money, Item* item);
 };
 
 class Monster : public Character
 {
 private:
-
+	int level;
 };
 
+class NPC : public Character
+{
+private:
+	std::string description;
 
+};
 #endif
